656c63d35bfb7476e657b4d5d7d97c27479c586d
Takaya-Sugino <takaya.sugino@kellogg.northwestern.edu>
2022-02-24 19:29:42 +0000

    change

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        entered_email = params["email"]
+        entered_password = params["password"]
+        # checking the email
+        @user = User.find_by({email: entered_email})
+        
+        if @user # yes, email matches
+            # check the password
+            if BCrypt::Password.new(@user.password) == entered_password
+                # yay! 
+                flash[:notice] = "Welcome!"
+                redirect_to "/companies"
+            else 
+                # password doesnt match!
+                flash[:notice] = "Password is incorect"
+                redirect_to "/sessions/new"
+            end 
+        else 
+            # email doesnt match, send back to the login page
+            flash[:notice] = "No user with that email address"
+            redirect_to "/sessions/new" 
+        end 
+    end
+
+end
